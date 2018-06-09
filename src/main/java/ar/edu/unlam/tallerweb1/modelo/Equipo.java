2f8e6923244111049ef0e0f6a2a09f82557a6d63
Daniel Marconi <marconidaniel@outlook.com>
2018-06-09 20:00:57 -0300

    Creacion de clases Equipo y Partido

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Equipo.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Equipo.java
@@ -1,7 +1,39 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
-/**
- * Created by dani on 09/06/18.
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Equipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String nombre3caracteres;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre3caracteres() {
+        return nombre3caracteres;
+    }
+
+    public void setNombre3caracteres(String nombre3caracteres) {
+        this.nombre3caracteres = nombre3caracteres;
+    }
 }
--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
@@ -19,8 +19,7 @@ public class Usuario {
 	// el atributo, la misma admite nulos, y el tipo de dato se deduce del tipo de dato de java.
 	private String email;
 	private String password;
-	private String rol;
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -38,13 +37,5 @@ public class Usuario {
 	}
 	public void setPassword(String password) {
 		this.password = password;
-	}
-
-	public String getRol() {
-		return rol;
-	}
-
-	public void setRol(String rol) {
-		this.rol = rol;
 	}
 }
