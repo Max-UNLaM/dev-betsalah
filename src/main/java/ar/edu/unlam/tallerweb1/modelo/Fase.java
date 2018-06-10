21e7248c5ad99711a881d8c9d6d3a08c94e6b1ac
Thargelion <elwindwalker@gmail.com>
2018-06-10 14:06:22 -0300

    Model Fase + test

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Fase.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Fase.java
@@ -1,4 +1,15 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Fase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    protected String tipo;
+    protected String nombre;
 }
--- src/test/java/ar/edu/unlam/tallerweb1/simulacion/JugarPartidoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/simulacion/JugarPartidoTest.java
@@ -1,4 +1,16 @@
 package ar.edu.unlam.tallerweb1.simulacion;
 
-public class JugarPartidoTest {
+import ar.edu.unlam.tallerweb1.SpringTest;
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+public class JugarPartidoTest extends SpringTest {
+
+    @Test
+    @Transactional
+    @Rollback
+    public void cargarGolesDevuelveResultado() {
+
+    }
 }
