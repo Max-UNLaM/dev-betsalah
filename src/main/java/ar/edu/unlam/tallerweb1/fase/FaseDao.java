7c529a06fa13eba5e2700d19f393356f1a6e892d
juanm <juanm@LAPTOP-MCU3KSPO>
2018-06-20 17:30:02 -0300

    vista

--- src/main/java/ar/edu/unlam/tallerweb1/fase/FaseDao.java
+++ src/main/java/ar/edu/unlam/tallerweb1/fase/FaseDao.java
@@ -6,7 +6,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 
 import java.util.List;
 
---
+++ src/main/java/ar/edu/unlam/tallerweb1/usuario/UsuarioServicio.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.tallerweb1.usuario;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServicio {
+	@Inject
+	private UsuarioDao usuarioDao;
+	
+	
+	
+}
