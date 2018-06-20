94f237f749e335ed39d17c01038a05dd442c5122
Thargelion <elwindwalker@gmail.com>
2018-06-20 18:00:29 -0300

    Equipo Service

---
+++ src/main/java/ar/edu/unlam/tallerweb1/equipo/EquipoCrud.java
@@ -0,0 +1,7 @@
+package ar.edu.unlam.tallerweb1.equipo;
+import ar.edu.unlam.tallerweb1.dao.Crud;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipoCrud extends Crud<Equipo, Long> {
+}
--- src/main/java/ar/edu/unlam/tallerweb1/equipo/EquipoDao.java
+++ src/main/java/ar/edu/unlam/tallerweb1/equipo/EquipoDao.java
@@ -1,9 +1,47 @@
 package ar.edu.unlam.tallerweb1.equipo;
 
-import ar.edu.unlam.tallerweb1.dao.Crud;
-import org.springframework.beans.factory.annotation.Qualifier;
+import ar.edu.unlam.tallerweb1.gol.Gol;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
+import javax.inject.Inject;
+import java.util.List;
+
 @Repository
-public interface EquipoDao extends Crud<Equipo, Long> {
+public class EquipoDao implements EquipoCrud {
+    @Inject
+    private SessionFactory sessionFactory;
+
+    @SuppressWarnings("unchecked")
+    public List<Equipo> list() {
+        final Session sesion = sessionFactory.getCurrentSession();
+        return sesion.createCriteria(Gol.class)
+                .list();
+    }
+
+    public Equipo update(Equipo gol) {
+        final Session sesion = sessionFactory.getCurrentSession();
+        sesion.update(gol);
+        return gol;
+    }
+
+    public Equipo read(Long id) {
+        final Session sesion = sessionFactory.getCurrentSession();
+        return sesion.get(Equipo.class, id);
+    }
+
+    public Equipo read(String nombre) {
+        final Session sesion = sessionFactory.getCurrentSession();
+        return (Equipo)sesion.createCriteria(Equipo.class)
+                .add(Restrictions.eq("nombre", nombre))
+                .uniqueResult();
+    }
+
+    public Equipo create (Equipo equipo) {
+        final Session sesion = sessionFactory.getCurrentSession();
+        sesion.save(equipo);
+        return equipo;
+    }
 }
