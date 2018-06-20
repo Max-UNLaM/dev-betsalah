6ee6c931b15afebb6fe4319ceb516d1c63472ca6
Thargelion <elwindwalker@gmail.com>
2018-06-20 19:02:18 -0300

    Preparacion

--- src/main/java/ar/edu/unlam/tallerweb1/preparacion/PreparacionService.java
+++ src/main/java/ar/edu/unlam/tallerweb1/preparacion/PreparacionService.java
@@ -1,4 +1,35 @@
 package ar.edu.unlam.tallerweb1.preparacion;
 
-public class PreparacionService {
+import ar.edu.unlam.tallerweb1.equipo.EquipoService;
+import ar.edu.unlam.tallerweb1.fase.FaseService;
+import ar.edu.unlam.tallerweb1.jugador.JugadorService;
+import ar.edu.unlam.tallerweb1.partido.PartidoService;
+import ar.edu.unlam.tallerweb1.service.CargaService;
+
+import javax.inject.Inject;
+
+public class PreparacionService implements CargaService {
+
+    @Inject
+    private FaseService faseService;
+
+    @Inject
+    private EquipoService equipoService;
+
+    @Inject
+    private PartidoService partidoService;
+
+    @Inject
+    private JugadorService jugadorService;
+
+    public PreparacionService(){
+        this.cargar();
+    }
+
+    public void cargar(){
+        faseService.cargar();
+        equipoService.cargar();
+        partidoService.cargar();
+        jugadorService.cargar();
+    }
 }
